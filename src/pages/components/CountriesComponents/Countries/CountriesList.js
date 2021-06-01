3d3d0dbd9625a83b98c109c8354531b57b1f9fa0
loretta-arineva <wowshifter@gmail.com>
2021-06-01 21:55:28 +0800

    Working on single page country

--- src/pages/components/CountriesComponents/Countries/CountriesList.js
+++ src/pages/components/CountriesComponents/Countries/CountriesList.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import CountryCard from '../../../components/CountriesComponents/Countries/CountryCard';
 
 const Countries = () => {
-    // const [allCountries, setAllCountries] = useState('');
+    const [allCountries, setAllCountries] = useState([]);
 
-    // useEffect(() => {
-    //     const countriesUrl = 'https://restcountries.eu/rest/v2/all';
+    useEffect(() => {
+        const countriesUrl = 'https://restcountries.eu/rest/v2/all';
 
-    //     axios.get(countriesUrl).then((countries) => {
-    //         setAllCountries(countries.data);
-    //         console.log(allCountries);
-    //     })
-    // }, [setAllCountries]);
+        axios.get(countriesUrl).then((countries) => {
+            setAllCountries(countries.data);
+            console.log(allCountries);
+        }, [setAllCountries]);
+    });
     return (
         <section id="country-list">
             <div className="country-grid">
-                {/* {allCountries.map((country) => {
+                {allCountries.map((country) => {
                     return (
-                        <CountryCard name={country.name} population={country.population} region={country.region} capital={country.capital} flag={country.flag} />
+                        <Link to={"/" + country.name} key={country.alpha3Code}><CountryCard name={country.name} population={country.population} region={country.region} capital={country.capital} flag={country.flag} /></Link>
                     )
-                })} */}
+                })}
             </div>
         </section>
     )
--- src/pages/containers/Country.js
+++ src/pages/containers/Country.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Country = () => {
+import { useParams } from 'react-router-dom';
+
+const Country = ({ location }) => {
+    const { country: countryID } = useParams();
+    const [country, setCountry] = useState('');
+    useEffect(() => {
+        const countryUrl = 'https://restcountries.eu/rest/v2/name/' + countryID;
+        axios.get(countryUrl).then(country => {
+            setCountry(country.data[0]);
+            console.log(country);
+        });
+    }, [setCountry]);
+
     return (
-        <div>
-            
-        </div>
+        <section id="country">
+            <div className="country-data__container">
+                <img src="" alt="" />
+                <div className="country-data">
+                    <h1>{country.name}</h1>
+                    <p>Native name: <span>{country.nativeName}</span></p>
+                    <p>Population: <span>{country.population}</span></p>
+                    <p>Region: <span>{country.region}</span></p>
+                    <p>Sub Region: <span>{country.subRegion}</span></p>
+                    <p>Capital: <span>{country.capital}</span></p>
+                    <p>Top Level Domain: <span>{country.topLevelDomain}</span></p>
+                    <p>Currencies: <span>
+                        {/* {country ? (
+                            country.currencies.map(currency => {
+                                return currency;
+                            })
+                        ) : ''} */}
+                    </span></p>
+                    {/* <p>Languages: <span>{country.languages.map(language => {return language;})}</span></p> */}
+                    <h3>Border Countries:</h3>
+                    {country ? (
+                        country.borders.map(country => {
+                            return country;
+                        })
+
+                    ) : ''}
+                </div>
+            </div>
+        </section>
     )
 }
 
