d8ce8f078fe384dafa87b28e1a2b07d78c5fa92e
loretta-arineva <wowshifter@gmail.com>
2021-06-02 09:29:48 +0800

    Fetched all data for single country

--- src/pages/containers/Country.js
+++ src/pages/containers/Country.js
@@ -6,34 +6,42 @@ import { useParams } from 'react-router-dom';
 const Country = ({ location }) => {
     const { country: countryID } = useParams();
     const [country, setCountry] = useState('');
+
     useEffect(() => {
         const countryUrl = 'https://restcountries.eu/rest/v2/name/' + countryID;
         axios.get(countryUrl).then(country => {
             setCountry(country.data[0]);
-            console.log(country);
+            console.log(country.data[0]);
         });
     }, [setCountry]);
 
     return (
         <section id="country">
             <div className="country-data__container">
-                <img src="" alt="" />
+                <img src={country.flag} alt="" />
                 <div className="country-data">
                     <h1>{country.name}</h1>
-                    <p>Native name: <span>{country.nativeName}</span></p>
-                    <p>Population: <span>{country.population}</span></p>
-                    <p>Region: <span>{country.region}</span></p>
-                    <p>Sub Region: <span>{country.subRegion}</span></p>
-                    <p>Capital: <span>{country.capital}</span></p>
-                    <p>Top Level Domain: <span>{country.topLevelDomain}</span></p>
-                    <p>Currencies: <span>
-                        {/* {country ? (
-                            country.currencies.map(currency => {
-                                return currency;
-                            })
-                        ) : ''} */}
-                    </span></p>
-                    {/* <p>Languages: <span>{country.languages.map(language => {return language;})}</span></p> */}
+
+                    <div className="main-data">
+                        <p>Native name: <span>{country.nativeName}</span></p>
+                        <p>Population: <span>{country.population}</span></p>
+                        <p>Region: <span>{country.region}</span></p>
+                        <p>Sub Region: <span>{country.subRegion}</span></p>
+                        <p>Capital: <span>{country.capital}</span></p>
+                    </div>
+
+                    <div className="sub-data">
+
+                        <p>Top Level Domain: <span>{country.topLevelDomain}</span></p>
+                        <p>Currencies: <span>
+                            {country ? (
+                                country.currencies.map(currency => {
+                                    return currency.name;
+                                })
+                            ) : ''}
+                        </span></p>
+                    </div>
+
                     <h3>Border Countries:</h3>
                     {country ? (
                         country.borders.map(country => {
